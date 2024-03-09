77fb7042bc61c971fbb06b0becd002bdcc6a41ab
Holokit MBP Dec.12 <holokitmbpdec.12@wanghandeMacBook-Pro-2.local>
2024-03-09 12:56:10 +0800

    asset commit

---
+++ Assets/ParticleCity/Scripts/Graphics/ParticleCityGPUInstancingRenderer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class ParticleCityGPUInstancingRenderer : MonoBehaviour
+{
+    public Mesh Mesh;
+    public Texture2D PositionTexture;
+    public Material Material;
+    public ParticleCityGenParams GenParams;
+
+    private List<Matrix4x4> matrices;
+    private MaterialPropertyBlock materialPropertyBlock;
+
+    void OnEnable() {
+        matrices = new List<Matrix4x4>(GenParams.InstanceCount);
+        for (int i = 0; i < GenParams.InstanceCount; i++)
+        {
+            matrices.Add(transform.localToWorldMatrix);
+        }
+
+        float[] offsets = new float[GenParams.InstanceCount];
+        for (int i = 0; i < GenParams.InstanceCount; i++)
+        {
+            offsets[i] = (float)(GenParams.RowsPerInstance * i) / PositionTexture.height;
+        }
+
+        materialPropertyBlock = new MaterialPropertyBlock();
+        materialPropertyBlock.SetFloatArray("_InstancingRowOffset", offsets);
+
+        Camera.onPreCull -= drawWithCamera;
+        Camera.onPreCull += drawWithCamera;
+    }
+ 
+    void OnDisable() {
+        Camera.onPreCull -= drawWithCamera;
+    }
+
+    private void drawWithCamera(Camera camera)
+    { 
+        if (Mesh == null || PositionTexture == null || GenParams == null)
+        {
+            return;
+        }
+
+        if (transform.localToWorldMatrix != matrices[0])
+        {
+            for (int i = 0; i < GenParams.InstanceCount; i++)
+            {
+                matrices[i] = transform.localToWorldMatrix;
+            }
+        }
+
+        Graphics.DrawMeshInstanced(
+            mesh: Mesh,
+            submeshIndex: 0,
+            material: Material,
+            matrices: matrices,
+            properties: materialPropertyBlock,
+            castShadows: ShadowCastingMode.Off,
+            receiveShadows: false,
+            layer: 0,
+            camera: camera,
+            lightProbeUsage: LightProbeUsage.Off
+            );
+
+    }
+}
---
+++ Assets/ParticleCity/Scripts/ParticleCityGenParams.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "ParticleCityGenParamsDef", menuName = "ScriptableObject/ParticleCityGenParams", order = 0)]
+
+public class ParticleCityGenParams : ScriptableObject
+{
+    public string GroupName = "untitled";
+
+    public float SamplePerCubeUnit = 0.0005f;
+
+    public float SamplePerSquareUnit = 100;
+    public float TriangleEdgeSamplePerUnit = 10;
+
+    public int TextureWidth = 2048;
+    public int TextureHeight = 2048;
+
+    public ParticleCityGenSampleMethod SampleMethod;
+    public ParticleCityGenMeshFormat MeshFormat;
+
+    public int InstanceCount;
+    public int RowsPerInstance;
+}
+
+public enum ParticleCityGenSampleMethod
+{
+    Surface,
+    Volume,
+}
+
+public enum ParticleCityGenMeshFormat
+{
+    WithGeometryShader,
+    NoGeometryShader,
+    GPUInstancing
+}
+
+public enum ParticleCityGenTextureSize
+{
+    TextureSize1024 = 1024,
+    TextureSize2048 = 2048,
+    TextureSize4096 = 4096
+}
